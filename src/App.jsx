2cd0e1ac92074fcf3f0a03853c4f993fc9b1a279
Matheus <seu.narrador.oficial@gmail.com>
2026-09-15 10:58:49 -0300

    feat: instalando dependências de toas e importando no APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom' 
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import sobre from './pages/sobre'
+import Produtos from './pages/Produtos'
+import Contato from './pages/Contato'
+
+
 const App = () => {
   return (
     <>
